04d351c815b29ce37e9faf47e9562378f8698aba
WillB97 <will@rudi.org.uk>
2026-04-10 09:36:39 +0100

    update apt sources

--- packer/scripts/base.sh
+++ packer/scripts/base.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -eux -o pipefail
 
+# Update apt sources
+cat >/etc/apt/sources.list <<EOF
+deb http://legacy.raspbian.org/raspbian/ buster main contrib non-free rpi
+#deb http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
+# Uncomment line below then 'apt-get update' to enable 'apt-get source'
+#deb-src http://raspbian.raspberrypi.org/raspbian/ buster main contrib non-free rpi
+EOF
+
 export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y update
